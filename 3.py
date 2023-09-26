6aaf9ae686af24a2841226c4b4d1bcc5232d859a
Anton <vorrian967@gmail.com>
2023-09-26 20:00:45 +0300

    New update

--- 3.py
+++ 3.py
@@ -1,1 +1,17 @@
-print(123)
+
+
+class Building:
+    def __init__(self, height, year) -> None:
+        self.height = height
+        self.year = year
+    
+    def change_height(self, height):
+        self.height = height
+
+    def get_attr(self):
+        print(f'Висота - {self.height}\nРік - {self.year}\n******')
+
+School = Building('100m', 1982)
+School.get_attr()
+School.change_height('50m')
+School.get_attr()
